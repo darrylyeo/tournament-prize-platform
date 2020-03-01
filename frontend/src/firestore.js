248abafae77bcd10fe564a3e29e0605f66720868
Darryl Yeo <dev@darryl-yeo.com>
2020-03-01 08:33:11 +0000

    [Sam] Add Firebase/Firestore

--- frontend/src/firestore.js
+++ frontend/src/firestore.js
@@ -1,1 +1,26 @@
 // Firestore
+const firebase = require('firebase')
+require('firebase/firestore')
+​
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+	apiKey: 'AIzaSyBPVmTlaV7pNdog3Uv_TjFPudss-T6lcmY',
+	authDomain: 'tourneyearney.firebaseapp.com',
+	projectId: 'tourneyearney'
+})
+​
+var db = firebase.firestore()
+​
+function addTournament(tournament){
+	db.collection('Tournaments').add(tournament)
+		.then(function(docRef) {
+		console.log('Document written with ID: ', docRef.id)
+		})
+		.catch(function(error) {
+		console.error('Error adding document: ', error)
+	})
+}
+
+export {
+	addTournament
+}
